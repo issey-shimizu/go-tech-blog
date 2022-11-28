b1a33f10d6d55e8dd8e25684b89f34617fe65a8f
shimizu-issei <isseichuo@gmail.com>
2022-11-28 20:56:16 +0900

    fix

--- 10picks/ABC081A - Placing Marbles.go
+++ 10picks/ABC081A - Placing Marbles.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var s string
-  	fmt.Scanf("%s", &s)
-	  slice := strings.Split(s, "")
-	  count := 0
-  	for i := 0; i < len(slice); i++ {
-    if slice[i] == "1" {
+    var num, a, b, c int
+	fmt.Scanf("%d", &num)
+	fmt.Scanf("%d %d %d", &a, &b, &c)
+	ary := [...]int{*&a,*&b,*&c}
+    num := len(ary)
+    count := 0
+  	for i := 0; i < num; i++ {
+      sum := a + b + c
+      split := sum / 2
+      if spilt % 2 == 0 {
 			count++
+	   }
 		}
   }
-	fmt.Printf("%d",count)
+  fmt.Printf("%d",count)
+
+if sss == 0{
+
+xq
+}
+
 }
