23daf2c02daa1e13b9201f9f8d058652882de728
shimizu-issei <isseichuo@gmail.com>
2022-12-01 19:05:21 +0900

    new

---
+++ 10picks/ABC088B - Card Game for Two.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	
+
+
+10 4 5
+	//配列から最大値を取得する
+	max := arr[0] //最大値を格納するための変数
+	for _, value := range arr {
+    if value > max {
+        max = value
+    }
+}
+
+
+
+
+
+}
