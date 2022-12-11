5d3255c210e65cd0336ff7c1e26e6b5f13a14982
shimizu-issei <isseichuo@gmail.com>
2022-12-11 12:56:10 +0900

    fix

--- 10picks/ABC0271A - 484558.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var n int
-	fmt.Scan(&n)
-	fmt.Printf("%02X\n", n)
-}
--- 10picks/ABC085A - Already 2018.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var str string
-	fmt.Scan(&str)
-	fmt.Println("2018" + str[4:])
-}
