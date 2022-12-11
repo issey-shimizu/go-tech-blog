47a9a27b98d58f258d520c71e9817b4b5d56486b
shimizu-issei <isseichuo@gmail.com>
2022-12-11 14:20:17 +0900

    fix

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-<<<<<<< HEAD
 	"log"
 	"os"
 
@@ -59,28 +58,6 @@ func main() {
 		e.Logger.Fatal("$PORT must be set")
 	}
 	e.Logger.Fatal(e.Start(":" + port))
-=======
-	"net/http"
-	"strconv"
-	"time"
-
-	"github.com/flosch/pongo2"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-)
-
-const tmplPath = "src/template/"
-
-var e = createMux()
-
-func main() {
-	e.GET("/", articleIndex)
-	e.GET("/new", articleNew)
-    e.GET("/:id", articleShow)
-    e.GET("/:id/edit", articleEdit)
-
-	e.Logger.Fatal(e.Start(":8080"))
->>>>>>> go-tech-blog/main
 }
 
 func createMux() *echo.Echo {
@@ -89,7 +66,6 @@ func createMux() *echo.Echo {
 	e.Use(middleware.Recover())
 	e.Use(middleware.Logger())
 	e.Use(middleware.Gzip())
-<<<<<<< HEAD
 	e.Use(middleware.CSRF())
 
 	e.Static("/css", "src/css")
@@ -131,66 +107,3 @@ func basicAuth() echo.MiddlewareFunc {
 		return false, nil
 	})
 }
-
-
-// repository.SetDB(db)はなんのため？
-=======
-	e.Static("/css", "src/css")
-	e.Static("/js", "src/js")
-	return e
-}
-
-func articleIndex(c echo.Context) error {
-	data := map[string]interface{}{
-		"Message": "Article Index",
-		"Now":     time.Now(),
-	}
-	return render(c, "article/index.html", data)
-}
-
-func articleNew(c echo.Context) error {
-    data := map[string]interface{}{
-      "Message": "Article New",
-      "Now":     time.Now(),
-    }
- 
-    return render(c, "article/new.html", data)
-  }
-
-  func articleShow(c echo.Context) error {
-    id, _ := strconv.Atoi(c.Param("id"))
- 
-    data := map[string]interface{}{
-      "Message": "Article Show",
-      "Now":     time.Now(),
-      "ID":      id,
-    }
- 
-    return render(c, "article/show.html", data)
-  }
-
-  func articleEdit(c echo.Context) error {
-    id, _ := strconv.Atoi(c.Param("id"))
- 
-    data := map[string]interface{}{
-      "Message": "Article Edit",
-      "Now":     time.Now(),
-      "ID":      id,
-    }
- 
-    return render(c, "article/edit.html", data)
-  }
-
-
-func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
-	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
-}
-
-func render(c echo.Context, file string, data map[string]interface{}) error {
-	b, err := htmlBlob(file, data)
-	if err != nil {
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	return c.HTMLBlob(http.StatusOK, b)
-}
->>>>>>> go-tech-blog/main
